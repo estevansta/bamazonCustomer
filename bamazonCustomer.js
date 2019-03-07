3eec5999ac57551ad21706e64cad919b0efad5bd
estevansta <cruz@therrb.co>
2019-03-06 20:38:46 -0600

    Connection fixed and working now

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -5,39 +5,20 @@ var inquirer = require("inquirer");
 var connection = mysql.createConnection({
   host: "localhost",
 
-  // my PORT 3306
+  // Your port; if not 3306
   port: 3306,
 
-  // username
-  user: "bamazon",
+  // Your username
+  user: "root",
 
-  // password
-  password: "",
+  // Your password
+  password: "googers123",
   database: "bamazon"
 });
 
-// test connection, if there's an error throw an err 
+// callback connection function to display error in CL if is not able to connect
 connection.connect(function(err) {
-    if (err) throw err;
-    inputItem();
+  if (err) throw err;
+  console.log("connected as id " + connection.threadId);
+  connection.end();
 });
-
-function inputItem() {
-    inquirer
-      .prompt({
-        name: "action",
-        type: "input",
-        message: "Input the ID of the item you wish to purchase"
-      })
-      // code used from previous activity top5000 songs on mysql
-      .then(function(answer) {
-        var query = "SELECT position, song, year FROM top5000 WHERE ?";
-        connection.query(query, { artist: answer.artist }, function(err, res) {
-          for (var i = 0; i < res.length; i++) {
-            console.log("Position: " + res[i].position + " || Song: " + res[i].song + " || Year: " + res[i].year);
-          }
-          inputItem();
-        });
-      });
-
-    };
